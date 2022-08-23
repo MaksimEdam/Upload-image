48d03732cfc2b1c91011dad579a1fe0945fe70f3
MaksimEdam <Edamenko6@gmail.com>
2022-08-23 10:13:38 +0300

    altiore npm

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,9 @@
+import FormAltiore from "../FormAltiore/FormAltiore";
 function App() {
   return (
     <div className="App">
       <h1>Привет</h1>
+      <FormAltiore />
     </div>
   );
 }
---
+++ src/components/FormAltiore/FormAltiore.jsx
@@ -0,0 +1,18 @@
+import React, { useCallback } from "react";
+
+import { Form } from "@altiore/form";
+
+const FormAltiore = () => {
+  const handleSubmit = useCallback((values) => {
+    console.log("Переменные формы:", values);
+  }, []);
+
+  return (
+    <Form onSubmit={handleSubmit}>
+      <input type="file" name="name" />
+      <button type="submit">Отправить</button>
+    </Form>
+  );
+};
+
+export default FormAltiore;
